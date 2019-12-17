5942747cb86ad696628faaa4a08500e47801327c
BC30138 <sasori.axele@gmail.com>
2019-12-17 20:17:04 +0300

    zip now

--- ServerlessComputing/GoogleCloud/src_client/client.py
+++ ServerlessComputing/GoogleCloud/src_client/client.py
@@ -12,16 +12,15 @@ TEST_URL = "http://127.0.0.1:8888/resize"
 def main():
     proc = subprocess.Popen(['sudo', 'gcloud', 'auth' ,'print-identity-token'],
     stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
     token, _ = proc.communicate()
     token = str(token)[2:-3]
 
-    # files = {'image': open(, mode='rb')}
     with open('data/igor-nikolaev.jpg', 'rb') as image_file:
         encoded_string = base64.b64encode(image_file.read())
-
     headers = {"Authorization": "bearer {}".format(token.strip())}
+
     response = requests.post(URL, data=encoded_string, headers=headers)
+
     with open('data/data.zip', 'wb') as f:
         f.write(base64.b64decode(response.text))
 
