5ef6ce92bf47533df7d5b6426f205e3facd02f6e
BC30138 <sasori.axele@gmail.com>
2019-12-19 19:29:32 +0300

    flags update

--- ServerlessComputing/GoogleCloud/src_client/client.py
+++ ServerlessComputing/GoogleCloud/src_client/client.py
@@ -9,43 +9,52 @@ import requests
 
 PROD_URL = "https://us-central1-polytech-lab.cloudfunctions.net/resize"
 TEST_URL = "http://127.0.0.1:8888/resize"
-URL: str
-INPUT_FILE_PATH: str
 
+class Client():
+    def __init__(self):
+        parser = argparse.ArgumentParser(
+            description="Client to send requests to resize image")
+        parser.add_argument("-i", "--input", action='store',
+                            type=str,
+                            help="Path to image to send",
+                            required=False,
+                            default='data/igor-nikolaev.jpg')
+        parser.add_argument("-o", "--output", action='store',
+                            type=str,
+                            help="Path to response" +
+                            "zip/extracted images directory",
+                            required=False, default='data/data.zip')
+        parser.add_argument("-e", "--extract",
+                            help="Flag to extract response zip",
+                            action='store_true', required=False)
+        parser.add_argument("-l", "--local", help="Flag to local testing",
+                            action='store_true', required=False)
+        self.args = parser.parse_args()
+        if self.args.local:
+            self.url = TEST_URL
+            self.headers = {}
+        else:
+            self.url = PROD_URL
+            proc = subprocess.Popen(['sudo', 'gcloud',
+                                     'auth', 'print-identity-token'],
+                                    stdout=subprocess.PIPE,
+                                    stderr=subprocess.PIPE)
+            token, _ = proc.communicate()
+            token = str(token)[2:-3]
+            self.headers = {"Authorization": "bearer {}".format(token.strip())}
 
-def argument_parser():
-    parser = argparse.ArgumentParser(description = "Description for my parser"
+    def post(self):
+        with open(self.args.input, 'rb') as image_file:
+            encoded_string = base64.b64encode(image_file.read())
 
-def main():
-    # url: str
-    # headers: dict
-    # input_path: str
-    # output_path: str
+        response = requests.post(self.url, data=encoded_string,
+                                 headers=self.headers)
 
-    # if len(sys.argv) == 3:
-    #     if sys.argv[1] == "local":
-    #         url = TEST_URL
-    #     else:
-    #         raise Exception("Incorrect arguments.")
-    #     headers = {}
+        if self.args.extract:
+            print("TBD")
+        else:
+            with open(self.args.output, 'wb') as output_file:
+                output_file.write(base64.b64decode(response.text))
 
-    # elif len(sys.argv) == 1:
-    #     url = PROD_URL
-    #     proc = subprocess.Popen(['sudo', 'gcloud',
-    #                              'auth', 'print-identity-token'],
-    #                             stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #     token, _ = proc.communicate()
-    #     token = str(token)[2:-3]
-    #     headers = {"Authorization": "bearer {}".format(token.strip())}
-    # else:
-    #     raise Exception("Incorrect arguments.")
-
-    with open('data/igor-nikolaev.jpg', 'rb') as image_file:
-        encoded_string = base64.b64encode(image_file.read())
-
-    response = requests.post(url, data=encoded_string, headers=headers)
-
-    with open('data/data.zip', 'wb') as output_file:
-        output_file.write(base64.b64decode(response.text))
-
-main()
+app = Client()
+app.post()
